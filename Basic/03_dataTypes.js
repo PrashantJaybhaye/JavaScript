75804130d8b473691ca7551721d2e948beb3acb9
Prashant <149985389+PrashantJaybhaye@users.noreply.github.com>
2024-12-31 21:50:25 +0000

    add Datatypes

---
+++ Basic/03_dataTypes.js
@@ -0,0 +1,20 @@
+// alert("Prashant") // we are using node.js not browser
+
+console.log(3 + 3)
+console.log("Prashant")
+
+
+ let name = "Prashant"
+ let age = 19
+ let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => unAssigned value
+
+console.log(typeof "Prashant");
+
