9e1ae2ab6aa177637e51e9843058ecd5e9664cb3
Prashant <149985389+PrashantJaybhaye@users.noreply.github.com>
2024-12-31 23:50:30 +0000

    conversion

---
+++ Basic/04_conversionOperation.js
@@ -0,0 +1,48 @@
+/*
+let count = "54";
+
+console.log(typeof count);
+console.log(typeof(count));
+
+let valueInNumber = Number(count)
+console.log(typeof valueInNumber);
+
+// "54" => 54
+// "54abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+console.log(typeof BooleanisLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Prashant" => true
+
+let num1 = 44
+
+let Stringnum = String(num1)
+console.log(Stringnum)
+console.log(typeof Stringnum);
+*/
+
+// ******************OPeration*******************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Prashant"
+let str2 = " Jaybhaye"
+let str3 = str1 + str2
+console.log(str3);
