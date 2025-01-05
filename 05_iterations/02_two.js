475fb04fba975294c505bd928e394854ee0c0050
Prashant <149985389+PrashantJaybhaye@users.noreply.github.com>
2025-01-05 13:57:54 +0000

    While & Do while

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,27 @@
+// While Loop
+
+let i = 0
+while (i <= 10) {
+    // console.log(`value of index is ${i}`);
+    i = i + 2;
+    // i++;
+}
+
+
+
+// let myArray = [" Flash ", ' Captian america ',' iron man ']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+// do while Loop 
+
+
+let Count = 2
+do {
+    console.log(`Point of Team'A' ${Count}`);
+    Count = Count + 2   
+} while (Count <= 10);
